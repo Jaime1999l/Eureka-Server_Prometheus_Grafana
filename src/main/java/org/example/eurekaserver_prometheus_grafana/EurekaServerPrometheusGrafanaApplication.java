07febe063d9a0b7087af8c6860aafb2d292ba395
Jaime Lopez Diaz <jlopedia@myuax.com>
2024-12-28 05:56:55 +0100

    Initial commit

---
+++ src/main/java/org/example/eurekaserver_prometheus_grafana/EurekaServerPrometheusGrafanaApplication.java
@@ -0,0 +1,13 @@
+package org.example.eurekaserver_prometheus_grafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerPrometheusGrafanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerPrometheusGrafanaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/eurekaserver_prometheus_grafana/EurekaServerPrometheusGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eurekaserver_prometheus_grafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerPrometheusGrafanaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
